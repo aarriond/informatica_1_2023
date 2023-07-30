32f3b037eec4b75a1a540493fbd548e2b919bbc4
Alexis Damian Arriondo <alexisd@frba.utn.edu.ar>
2023-07-30 16:32:14 -0300

    321

---
+++ funciones.h
@@ -0,0 +1,35 @@
+//Parte 1
+void devolverNumeroAleatorio(int, int, unsigned int);
+void imprimeFloatByteAByte(float *);
+void devolverMediana(float *);
+void devolverMedia(float *);
+void devolverModa(float *);
+void devolverMayor(float *);
+void devolverMenor(float *);
+void copiarSoloPrimos(int *, int *);
+
+//Parte 2
+invertirString(arg1, arg2, arg3);
+removerEspacios(&var1);
+convertirAMayusculas(array1);
+convertirAMinusculas(array1);
+convertirACamelCase(array1);
+encontrarCaracter(array1);
+removerCaracter(array1);
+desplazarNPosiciones(array1, array2);
+
+//Parte 3
+devolverStringBinarioComoNumero(arg1, arg2, arg3);
+devolverStringOctalComoNumero(&var1);
+devolverStringHexaComoNumero(array1);
+imprimirNumeroEnBases(array1);
+imprimirNumerBaseNComoString(array1);
+
+//Parte 4
+
+//Parte 5
+int desplazar_letras (char*,char *);
+void desencriptar_texto (char*,char*,int);
+void Encriptar_Numeros (char*,char*);
+void Desencriptar_numero (char*, char*);
+int es_encriptable (char *);
---
+++ main.c
@@ -0,0 +1,298 @@
+#include <stdio.h>
+#define ARRAY_MAX 50
+
+int main (void){
+    
+    unsigned int opcion = 0;
+    unsigned int retorno = 0;
+    
+    printf("Ingrese una opción: \n");
+    printf("1 - Parte 1\n");
+    printf("2 - Parte 2\n");
+    printf("3 - Parte 3\n");
+    printf("4 - Parte 4\n");
+    printf("5 - Parte 5\n");
+    printf("0 - salir del programa\n");
+
+    scanf("%d", opcion);
+
+    switch(opcion){
+        case 1:
+            printf("Parte 1 - ingrese un ejercicio: \n");
+            printf("1\n");
+            printf("2\n");
+            printf("3\n");
+            printf("4\n");
+            printf("5\n");
+            printf("6\n");
+            printf("7\n");
+            printf("8\n");
+            scanf("%d", opcion);
+            switch(opcion){
+                case 1:
+                    int arg1, arg2;
+                    unsigned int arg3;
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg1);
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg2);
+                    printf("Ingrese un tipo de intervalo: \n");
+                    printf("1 - cerrado \n");
+                    printf("2 - abierto a izquierda\n");
+                    printf("3 - abierto a derecha\n");
+                    printf("4 - abierto\n");
+                    scanf("%d", arg3);
+                    devolverNumeroAleatorio(arg1, arg2, arg3);
+                    break;
+                case 2:
+                    float var1;
+                    printf("Ingrese un número real: \n");
+                    scanf("%f", &arg1);
+                    imprimeFloatByteAByte(&var1);
+                    break;
+                case 3:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverMediana(array1);
+                    break;
+                case 4:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverMedia(array1);
+                    break;
+                case 5:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverModa(array1);
+                    break;
+                case 6:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverMayor(array1);
+                    break;
+                case 7:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverMenor(array1);
+                    break;
+                case 8:
+                    int array1[ARRAY_MAX];
+                    int array2[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    for (int i = 0; i < 50; i++){
+                        array2[i] = 0;
+                    }
+                    copiarSoloPrimos(array1, array2);
+                    break;
+            }
+        case 2:
+            printf("Parte 2 - ingrese un ejercicio: \n");
+            printf("1\n");
+            printf("2\n");
+            printf("3\n");
+            printf("4\n");
+            printf("5\n");
+            printf("6\n");
+            printf("7\n");
+            printf("8\n");
+            scanf("%d", opcion);
+            switch(opcion){
+                case 1:
+                    int arg1, arg2;
+                    unsigned int arg3;
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg1);
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg2);
+                    printf("Ingrese un tipo de intervalo: \n");
+                    printf("1 - cerrado \n");
+                    printf("2 - abierto a izquierda\n");
+                    printf("3 - abierto a derecha\n");
+                    printf("4 - abierto\n");
+                    scanf("%d", arg3);
+                    invertirString(arg1, arg2, arg3);
+                    break;
+                case 2:
+                    float var1;
+                    printf("Ingrese un número real: \n");
+                    scanf("%f", &arg1);
+                    removerEspacios(&var1);
+                    break;
+                case 3:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    convertirAMayusculas(array1);
+                    break;
+                case 4:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    convertirAMinusculas(array1);
+                    break;
+                case 5:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    convertirACamelCase(array1);
+                    break;
+                case 6:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    encontrarCaracter(array1);
+                    break;
+                case 7:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    removerCaracter(array1);
+                    break;
+                case 8:
+                    int array1[ARRAY_MAX];
+                    int array2[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    for (int i = 0; i < 50; i++){
+                        array2[i] = 0;
+                    }
+                    desplazarNPosiciones(array1, array2);
+                    break;
+            }
+            break;
+        case 3:
+        printf("Parte 1 - ingrese un ejercicio: \n");
+        printf("1\n");
+        printf("2\n");
+        printf("3\n");
+        printf("4\n");
+        printf("5\n");
+        scanf("%d", opcion);
+            switch(opcion){
+                case 1:
+                    int arg1, arg2;
+                    unsigned int arg3;
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg1);
+                    printf("Ingrese un número entero: \n");
+                    scanf("%d", &arg2);
+                    printf("Ingrese un tipo de intervalo: \n");
+                    printf("1 - cerrado \n");
+                    printf("2 - abierto a izquierda\n");
+                    printf("3 - abierto a derecha\n");
+                    printf("4 - abierto\n");
+                    scanf("%d", arg3);
+                    devolverStringBinarioComoNumero(arg1, arg2, arg3);
+                    break;
+                case 2:
+                    float var1;
+                    printf("Ingrese un número real: \n");
+                    scanf("%f", &arg1);
+                    devolverStringOctalComoNumero(&var1);
+                    break;
+                case 3:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    devolverStringHexaComoNumero(array1);
+                    break;
+                case 4:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    imprimirNumeroEnBases(array1);
+                    break;
+                case 5:
+                    float array1[ARRAY_MAX];
+                    unsigned int varControl = 0;
+                    for (int i = 0; i < 50 || varControl != 0; i++){
+                        printf("Ingrese un valor para el array (\"-1\" para finalizar, array máximo configurado en 50 elementos): \n");
+                        scanf("%f", array1[i]);
+                        varControl = array1[i];
+                    }
+                    imprimirNumerBaseNComoString(array1);
+                    break;
+            }
+            break;
+        case 4:
+            printf("Enunciado A\n");
+            controlDeVentas();
+            break;
+        case 5:
+            printf("Enunciado B\n");
+            funcionesEncriptado();
+            break;
+        case 0:
+            retorno = 1;
+            break;
+    }
+
+    if(retorno==1){
+        printf("Salida del programa\n\n");
+        return 0;
+    }
+
+    return 0;
+}
