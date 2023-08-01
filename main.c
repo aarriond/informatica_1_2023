d0387b12f63ed0482d75ead4a1ade10038ecfc1f
Alexis Damian Arriondo <alexisd@frba.utn.edu.ar>
2023-07-31 22:22:33 -0300

    Continuar

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "funciones.h"
 #define ARRAY_MAX 50
 
 int main (void){
@@ -14,7 +15,7 @@ int main (void){
     printf("5 - Parte 5\n");
     printf("0 - salir del programa\n");
 
-    scanf("%d", opcion);
+    scanf("%u", &opcion);
 
     switch(opcion){
         case 1:
@@ -27,7 +28,7 @@ int main (void){
             printf("6\n");
             printf("7\n");
             printf("8\n");
-            scanf("%d", opcion);
+            scanf("%d", &opcion);
             switch(opcion){
                 case 1:
                     int arg1, arg2;
@@ -125,22 +126,13 @@ int main (void){
             printf("6\n");
             printf("7\n");
             printf("8\n");
-            scanf("%d", opcion);
+            scanf("%d", &opcion);
             switch(opcion){
                 case 1:
-                    int arg1, arg2;
-                    unsigned int arg3;
-                    printf("Ingrese un número entero: \n");
-                    scanf("%d", &arg1);
-                    printf("Ingrese un número entero: \n");
-                    scanf("%d", &arg2);
-                    printf("Ingrese un tipo de intervalo: \n");
-                    printf("1 - cerrado \n");
-                    printf("2 - abierto a izquierda\n");
-                    printf("3 - abierto a derecha\n");
-                    printf("4 - abierto\n");
-                    scanf("%d", arg3);
-                    invertirString(arg1, arg2, arg3);
+                    char *ptrString;
+                    printf("Ingrese un string para invertir: \n");
+                    ptrString = fgets();
+                    invertirString(*ptrString);
                     break;
                 case 2:
                     float var1;
@@ -235,7 +227,7 @@ int main (void){
                     printf("2 - abierto a izquierda\n");
                     printf("3 - abierto a derecha\n");
                     printf("4 - abierto\n");
-                    scanf("%d", arg3);
+                    scanf("%u", &arg3);
                     devolverStringBinarioComoNumero(arg1, arg2, arg3);
                     break;
                 case 2:
@@ -278,11 +270,11 @@ int main (void){
             break;
         case 4:
             printf("Enunciado A\n");
-            controlDeVentas();
+            //menu control de ventas;
             break;
         case 5:
             printf("Enunciado B\n");
-            funcionesEncriptado();
+            //menu encriptado;
             break;
         case 0:
             retorno = 1;
